6db30ae7392f4580097da5965e7b4837e9113f08
Caio <caiobarreto1acim@gmail.com>
2024-10-17 01:15:04 -0300

    Resolvendo conflitos

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from 'react'
 import { Main } from './components/Main/header.jsx'
+<<<<<<< HEAD
+=======
+import { About } from './components/About/about.jsx'
+>>>>>>> 4259c40 (feat: ABOUT READY)
 
 function App() {
 
   return (
     <>
     <Main />
+<<<<<<< HEAD
+=======
+    <About/>
+>>>>>>> 4259c40 (feat: ABOUT READY)
     </>
   )
 }
