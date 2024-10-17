e446a284026bf30cde73f6b78fd4933177b88d87
Caio <caiobarreto1acim@gmail.com>
2024-10-17 01:38:18 -0300

    feat: all right its about

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import { Main } from './components/Main/header.jsx'
+import { About } from './components/About/about.jsx'
 
 function App() {
 
   return (
     <>
     <Main />
+    <About />
     </>
   )
 }
